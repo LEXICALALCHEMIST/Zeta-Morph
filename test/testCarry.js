022e6cb51c49d4cc9296ed7cba2872d6bcc721d7
LEXICALALCHEMIST <HOPEASI369@GMAIL.COM>
2025-05-31 05:55:44 -0700

    Moving To SnapShot

--- test/testCarry.js
+++ test/testCarry.js
@@ -26,6 +26,72 @@ const tests = [
       numberLength: 3,
       activeUnitTarget: 'u3'
     }
+  },
+  {
+    description: 'Set skeleton to 1005 and push 5 (test carry propagation)',
+    operation: { initialSkeleton: 1005, pushValue: 5 },
+    expected: {
+      units: [
+        { currentSymbol: SYMBOL_SEQUENCE[1] },  // U1: ● (1)
+        { currentSymbol: SYMBOL_SEQUENCE[0] },  // U2: ⚙ (0)
+        { currentSymbol: SYMBOL_SEQUENCE[1] },  // U3: ● (1)
+        { currentSymbol: SYMBOL_SEQUENCE[0] },  // U4: ⚙ (0)
+        { currentSymbol: VOID_SYMBOL },         // U5: ⊙
+        { currentSymbol: VOID_SYMBOL },         // U6: ⊙
+        { currentSymbol: VOID_SYMBOL },         // U7: ⊙
+        { currentSymbol: VOID_SYMBOL },         // U8: ⊙
+        { currentSymbol: VOID_SYMBOL },         // U9: ⊙
+        { currentSymbol: VOID_SYMBOL },         // U10: ⊙
+        { currentSymbol: VOID_SYMBOL },         // U11: ⊙
+        { currentSymbol: VOID_SYMBOL }          // U12: ⊙
+      ],
+      numberLength: 4,
+      activeUnitTarget: 'u4'
+    }
+  },
+  {
+    description: 'Set skeleton to 100155 and push 5 (test carry propagation)',
+    operation: { initialSkeleton: 100155, pushValue: 5 },
+    expected: {
+      units: [
+        { currentSymbol: SYMBOL_SEQUENCE[1] },  // U1: ● (1)
+        { currentSymbol: SYMBOL_SEQUENCE[0] },  // U2: ⚙ (0)
+        { currentSymbol: SYMBOL_SEQUENCE[0] },  // U3: ⚙ (0)
+        { currentSymbol: SYMBOL_SEQUENCE[1] },  // U4: ● (1)
+        { currentSymbol: SYMBOL_SEQUENCE[6] },  // U5: • (6)
+        { currentSymbol: SYMBOL_SEQUENCE[0] },  // U6: ⚙ (0)
+        { currentSymbol: VOID_SYMBOL },         // U7: ⊙
+        { currentSymbol: VOID_SYMBOL },         // U8: ⊙
+        { currentSymbol: VOID_SYMBOL },         // U9: ⊙
+        { currentSymbol: VOID_SYMBOL },         // U10: ⊙
+        { currentSymbol: VOID_SYMBOL },         // U11: ⊙
+        { currentSymbol: VOID_SYMBOL }          // U12: ⊙
+      ],
+      numberLength: 6,
+      activeUnitTarget: 'u6'
+    }
+  },
+  {
+    description: 'Set skeleton to 199999 and push 1 (test multiple carry propagation)',
+    operation: { initialSkeleton: 199999, pushValue: 1 },
+    expected: {
+      units: [
+        { currentSymbol: SYMBOL_SEQUENCE[2] },  // U1: ○ (2)
+        { currentSymbol: SYMBOL_SEQUENCE[0] },  // U2: ⚙ (0)
+        { currentSymbol: SYMBOL_SEQUENCE[0] },  // U3: ⚙ (0)
+        { currentSymbol: SYMBOL_SEQUENCE[0] },  // U4: ⚙ (0)
+        { currentSymbol: SYMBOL_SEQUENCE[0] },  // U5: ⚙ (0)
+        { currentSymbol: SYMBOL_SEQUENCE[0] },  // U6: ⚙ (0)
+        { currentSymbol: VOID_SYMBOL },         // U7: ⊙
+        { currentSymbol: VOID_SYMBOL },         // U8: ⊙
+        { currentSymbol: VOID_SYMBOL },         // U9: ⊙
+        { currentSymbol: VOID_SYMBOL },         // U10: ⊙
+        { currentSymbol: VOID_SYMBOL },         // U11: ⊙
+        { currentSymbol: VOID_SYMBOL }          // U12: ⊙
+      ],
+      numberLength: 6,
+      activeUnitTarget: 'u6'
+    }
   }
 ];
 
